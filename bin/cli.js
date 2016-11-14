1fcbf2593636b081deac8db68e03e909bf5ecd14
Chris Helgert <chris.helgert@yahoo.com>
2016-11-14 12:30:32 +0100

    update-notifier for new caniuse-yarn-version in npm-artifactory

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 const programm = require('commander');
+const updateNotifier = require('update-notifier');
 const caniuseYarn = require('../lib');
 const pkg = require('../package.json');
+
+// notify on new version
+updateNotifier({ pkg }).notify({ defer: false });
 
 programm
   .version(pkg.version)
