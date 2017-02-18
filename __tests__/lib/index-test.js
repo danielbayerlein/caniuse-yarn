db4f9619a07d99def75ebca7e933a28ce2961e6a
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-02-18 10:36:22 +0100

    Remove eslint-ignore and use test() instead of it()

--- __tests__/lib/index-test.js
+++ __tests__/lib/index-test.js
@@ -1,17 +1,17 @@
-describe('caniuseYarn', () => {
-  const caniuseYarn = require('../../lib'); // eslint-disable-line global-require
-  const shelljs = require('shelljs'); // eslint-disable-line global-require
+const shelljs = require('shelljs');
+const caniuseYarn = require('../../lib');
 
+describe('caniuseYarn', () => {
   beforeEach(() => {
     shelljs.which = jest.fn();
   });
 
-  it('returns true if yarn is available', () => {
+  test('returns true if yarn is available', () => {
     shelljs.which.mockReturnValue(true);
     expect(caniuseYarn()).toBeTruthy();
   });
 
-  it('returns false if yarn is not available', () => {
+  test('returns false if yarn is not available', () => {
     shelljs.which.mockReturnValue(false);
     expect(caniuseYarn()).toBeFalsy();
   });
